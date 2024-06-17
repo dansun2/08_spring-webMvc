a5a56a81e6ff97b52c52a01d54694034d8042d9b
dansun2 <dansunmusik7@gmail.com>
2024-06-17 18:08:24 +0900

    240617

--- test-app-flashattribute/src/main/java/com/ohgiraffers/testappflashattribute/controller/ResolverController.java
+++ test-app-flashattribute/src/main/java/com/ohgiraffers/testappflashattribute/controller/ResolverController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping
@@ -31,18 +32,26 @@ public class ResolverController {
     }
 
     @GetMapping("result2")
-    public String result2(Model model, @RequestParam("selection") String value, ModelAndView mv){
-        model.addAttribute("message", "거지 리다이렉트");
-        model.addAttribute("selection", value);
-        return "/result1";
-    }
+    public ModelAndView result2(Model model, String selection, @RequestParam("selection") String value, ModelAndView mv, RedirectAttributes rttr){
+        if("poor".equals(selection)) {
 
-    @GetMapping("goodSelect")
-    public ModelAndView GoodSelect(@RequestParam("selection") String value, ModelAndView mv){
-        mv.addObject("selection", value);
-        mv.addObject("selection", value);
-        mv.setViewName("redirect:/");
+            //리다이렉트 이후에 사용할 속성을 추가
+            rttr.addFlashAttribute("result2", "거지입니다.");
+            mv.setViewName("redirect:/");
+            return mv;
+        }
+        mv.addObject("selection", selection); // 뭘 추가하는거지?
+        mv.addObject("message", value); // 뭘 추가하는거지?
+        mv.setViewName("result1");
         return mv;
     }
+
+//    @GetMapping("goodSelect")
+//    public ModelAndView GoodSelect(@RequestParam("selection") String value, ModelAndView mv){
+//        mv.addObject("selection", value);
+//        mv.addObject("selection", value);
+//        mv.setViewName("redirect:/");
+//        return mv;
+//    }
 
 }
