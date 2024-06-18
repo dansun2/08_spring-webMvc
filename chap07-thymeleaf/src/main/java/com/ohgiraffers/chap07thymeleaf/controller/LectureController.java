bc3efedefc2953b4bf1e5c6f03c3bdf0051dc92a
dansun2 <dansunmusik7@gmail.com>
2024-06-18 10:18:11 +0900

    240618

--- chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
+++ chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
@@ -1,9 +1,23 @@
 package com.ohgiraffers.chap07thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+
+//lecture라는 요청이 들어오면 이 밑에 클래스에서 찾겠다.
 @RequestMapping("lecture")
 public class LectureController {
+
+    // get 방식의 요청이 들어오면 expression이 있는지 찾고 있으면 밑에 return 실행
+    @RequestMapping(value = "expression", method = RequestMethod.GET) // 축약하면 @GetMapping("expression")
+    public ModelAndView expression(ModelAndView mv){
+
+        return mv;
+    }
+
+
 }
