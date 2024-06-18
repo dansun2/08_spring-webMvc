b400e2a333f93a743cb1793a05a802306c501b4e
dansun2 <dansunmusik7@gmail.com>
2024-06-18 10:09:28 +0900

    240618

---
+++ chap07-thymeleaf/src/main/java/com/ohgiraffers/chap07thymeleaf/controller/LectureController.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.chap07thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("lecture")
+public class LectureController {
+}
