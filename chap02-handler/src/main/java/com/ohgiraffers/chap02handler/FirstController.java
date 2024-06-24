6158a1d9a53f4d94a059f52b390ca31bdaf9eef2
dansun2 <dansunmusik7@gmail.com>
2024-06-24 11:16:20 +0900

    240624

--- chap02-handler/src/main/java/com/ohgiraffers/chap02handler/FirstController.java
+++ chap02-handler/src/main/java/com/ohgiraffers/chap02handler/FirstController.java
@@ -2,7 +2,10 @@ package com.ohgiraffers.chap02handler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/first/")
@@ -18,5 +21,29 @@ public class FirstController {
     * */
     @GetMapping("regist")
     public void regist(){
+    }
+
+    /*
+    * 1. WebRequest로 요청 파라미터 전달 받기
+    *   파라미터 선언부에 WebRequest 타입을 선언하면 해당 메소드 호출 시 인자로 값을 전달해준다.
+    *   핸들러 메소드 매개변수로 HttpServletRequest HttpServletResponse도 사용 가능하다.
+    *   상위 타입인 ServletRequest ServletResponse도 사용 가능하다.
+    *   WebRequest는 HttpServletRequest의 요청 정보를 거의 대부분 그대로 가지고 있는 API로 Servlet에 종속적이지 않다.
+    *   HttpServlet은 Servlet API의 일부이고,
+    *   WebRequest는 spring의 일부이기 때문에 spring 기반의 프로젝트에서 더 자주 사용된다.
+    * */
+
+    @PostMapping("regist")
+    public ModelAndView regist(ModelAndView modelAndView, WebRequest webRequest) {
+        String name = webRequest.getParameter("name");
+        int price = Integer.parseInt(webRequest.getParameter("price"));
+        int category = Integer.parseInt(webRequest.getParameter("category"));
+
+        String message = name + "을 신규 메뉴 목록의 " + category + "번 카테고리에 " + price + "원으로 등록하였습니다.";
+        System.out.println(message);
+
+        modelAndView.addObject("message", message);
+        modelAndView.addObject("/first/messagePrinter");
+        return modelAndView;
     }
 }
