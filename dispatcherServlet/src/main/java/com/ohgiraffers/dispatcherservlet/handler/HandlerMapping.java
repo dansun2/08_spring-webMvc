421f63d30dc0bf541cec5579ab4531afbccf56b1
dansun2 <dansunmusik7@gmail.com>
2024-07-10 15:10:28 +0900

    240710

--- dispatcherServlet/src/main/java/com/ohgiraffers/dispatcherservlet/handler/HandlerMapping.java
+++ dispatcherServlet/src/main/java/com/ohgiraffers/dispatcherservlet/handler/HandlerMapping.java
@@ -1,5 +1,18 @@
 package com.ohgiraffers.dispatcherservlet.handler;
 
-public class HandlerMapping {
+import com.ohgiraffers.dispatcherservlet.controller.Controller;
 
+public class HandlerMapping {
+    public Controller getController(String url) {
+        Object controller = null;
+        String type = null;
+
+        if(url.equals("/test")){
+            type = "TestController";
+        }else if(url.equals("/main")){
+            type = "MainController";
+        }else if(url.equals("/redirect")){
+            type = "RedirectController";
+        }
+    }
 }
