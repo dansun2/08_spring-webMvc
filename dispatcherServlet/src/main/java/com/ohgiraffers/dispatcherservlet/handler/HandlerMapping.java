6e4c4187efd1bc86c89b165e6dadaf142259c3d2
dansun2 <dansunmusik7@gmail.com>
2024-07-10 15:37:28 +0900

    240710

--- dispatcherServlet/src/main/java/com/ohgiraffers/dispatcherservlet/handler/HandlerMapping.java
+++ dispatcherServlet/src/main/java/com/ohgiraffers/dispatcherservlet/handler/HandlerMapping.java
@@ -1,6 +1,11 @@
 package com.ohgiraffers.dispatcherservlet.handler;
 
 import com.ohgiraffers.dispatcherservlet.controller.Controller;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class HandlerMapping {
     public Controller getController(String url) {
@@ -13,6 +18,26 @@ public class HandlerMapping {
             type = "MainController";
         }else if(url.equals("/redirect")){
             type = "RedirectController";
+        }
+
+        // 현재 패키지경로 하위에 있는 클래스, 메서드, 필드 등을 모두 가져오겠다.
+        try(ScanResult scanResult = new ClassGraph().enableAllInfo().acceptPackages("com.ohgiraffers.dispatcherservlet").scan()){
+            // 얘를 구현한 구현체의 목록들(상속받는 애들)을 classInfos로 받아오겠다.
+            ClassInfoList classInfos = scanResult.getClassesImplementing("com.ohgiraffers.dispatcherservlet.controller.Controller");
+            for(Class<?> implClass : classInfos.loadClasses()){
+                if(implClass.getName().equals("com.ohgiraffers.dispatcherservlet.controller." + type)){
+                    controller = implClass.getDeclaredConstructor().newInstance();
+
+                }
+            }
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
     }
 }
