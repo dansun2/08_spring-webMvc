22f92ba51084e693c496025265490a50aea51c4d
dansun2 <dansunmusik7@gmail.com>
2024-07-11 11:21:38 +0900

    240711

---
+++ chap04-exception/src/main/java/com/ohgiraffers/chap04exception/ExceptionHandlerController.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.chap04exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExceptionHandlerController {
+
+    @GetMapping("controller-null")
+    public String nullPointerExceptionTest(){
+        String str = null;
+
+        System.out.println(str.charAt(0)); // str은 null이니까 chatAt(0)을 하면 널포인트 익셉션이 발생함(없는 값을 가져오려고 하니까)
+        return "/";
+    }
+
+    @ExceptionHandler(NullPointerException.class) // 이 클래스 내부에서 널포인트 익셉션이 발생하면 내가 여기서 처리하겠다
+    public String nullPointerExceptionHandler(NullPointerException exception){
+        System.out.println("controller 레벨의 exception 처리");
+        return "error/nullpointer";
+    }
+}
