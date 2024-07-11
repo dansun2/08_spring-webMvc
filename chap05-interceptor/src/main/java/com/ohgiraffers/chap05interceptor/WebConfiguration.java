6c167abe13ac2d59cdbdc0c8efe4ce68a1185a76
dansun2 <dansunmusik7@gmail.com>
2024-07-11 10:48:36 +0900

    240711

--- chap05-interceptor/src/main/java/com/ohgiraffers/chap05interceptor/WebConfiguration.java
+++ chap05-interceptor/src/main/java/com/ohgiraffers/chap05interceptor/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.chap05interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,16 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     public WebConfiguration(StopWatchInterceptor stopWatchInterceptor) {
         this.stopWatchInterceptor = stopWatchInterceptor;
+    }
+
+    @Override // 인터셉터를 등록시켜주는 부분
+    public void addInterceptors(InterceptorRegistry registry) {
+         registry.addInterceptor(stopWatchInterceptor)
+                 .addPathPatterns("/*") // 어떤 요청이 들어왔을때 인터셉터를 동작시키겠느냐
+                 .excludePathPatterns("/css/**")
+                 .excludePathPatterns("/images/**")
+                 .excludePathPatterns("/js/**")
+                 .excludePathPatterns("/error");
+
     }
 }
