092ef82eba4db100d55963239c7cfadc9ec351fc
dansun2 <dansunmusik7@gmail.com>
2024-07-11 10:32:44 +0900

    240711

--- chap05-interceptor/src/main/java/com/ohgiraffers/chap05interceptor/StopWatchInterceptor.java
+++ chap05-interceptor/src/main/java/com/ohgiraffers/chap05interceptor/StopWatchInterceptor.java
@@ -1,7 +1,10 @@
 package com.ohgiraffers.chap05interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 /*
 * 핸들러 인터셉터 상속받기
@@ -12,5 +15,20 @@ public class StopWatchInterceptor implements HandlerInterceptor {
 
     public StopWatchInterceptor(MenuService menuService) {
         this.menuService = menuService;
+    }
+
+    @Override // 전처리를 위한 핸들러 filter -> interceptor -> controller
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override // 후처리를 위한 핸들러 filter -> controller -> interceptor
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
